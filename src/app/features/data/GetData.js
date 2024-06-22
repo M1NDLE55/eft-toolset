2628522685f37505b5304d29c725668d32fce9a1
Ewan <108713859+M1NDLE55@users.noreply.github.com>
2024-06-22 18:48:36 +0200

    item query

---
+++ src/app/features/data/GetData.js
@@ -0,0 +1,23 @@
+export default async function GetData(query) {
+  try {
+    const response = await fetch("https://api.tarkov.dev/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        query: query,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok" + response.statusText);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+}
