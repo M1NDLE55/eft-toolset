ad576b54b2b24e1c99be6ff49c8eceac4f2d3bfa
Ewan <108713859+M1NDLE55@users.noreply.github.com>
2024-07-11 17:43:02 +0200

    suspense fix

--- src/app/scanner/page.js
+++ src/app/scanner/page.js
@@ -6,32 +6,46 @@ import SearchBar from "../components/scanner/SearchBar";
 import ItemSelection from "../components/scanner/ItemSelection";
 import SelectedItem from "../components/scanner/SelectedItem";
 
-export default function Page() {
-  const pathname = usePathname();
+function Item({ setItemSearch, filteredItems, setFilteredItems, pathname }) {
   const searchParams = useSearchParams();
-  const [itemSearch, setItemSearch] = useState("");
-  const [filteredItems, setFilteredItems] = useState(null);
 
   return (
-    <Suspense>
-      <main className="flex justify-center items-center p-4">
-        <div className="max-w-xl w-full flex flex-col gap-4">
-          <SearchBar
-            itemSearch={itemSearch}
-            setItemSearch={setItemSearch}
-            setFilteredItems={setFilteredItems}
-            pathname={pathname}
-          />
-          <ItemSelection
+    <>
+      <ItemSelection
+        setItemSearch={setItemSearch}
+        filteredItems={filteredItems}
+        searchParams={searchParams}
+        setFilteredItems={setFilteredItems}
+        pathname={pathname}
+      />
+      <SelectedItem searchParams={searchParams} />
+    </>
+  );
+}
+
+export default function Page() {
+  const pathname = usePathname();
+  const [filteredItems, setFilteredItems] = useState(null);
+  const [itemSearch, setItemSearch] = useState("");
+
+  return (
+    <main className="flex justify-center items-center p-4">
+      <div className="max-w-xl w-full flex flex-col gap-4">
+        <SearchBar
+          itemSearch={itemSearch}
+          setItemSearch={setItemSearch}
+          setFilteredItems={setFilteredItems}
+          pathname={pathname}
+        />
+        <Suspense>
+          <Item
             setItemSearch={setItemSearch}
             filteredItems={filteredItems}
-            searchParams={searchParams}
             setFilteredItems={setFilteredItems}
             pathname={pathname}
           />
-          <SelectedItem searchParams={searchParams} />
-        </div>
-      </main>
-    </Suspense>
+        </Suspense>
+      </div>
+    </main>
   );
 }
