93bc10675e739cab03e472034c9bf1984847d457
Ewan <108713859+M1NDLE55@users.noreply.github.com>
2024-08-24 15:13:50 +0200

    Fetch items in root layout

--- src/app/components/root/FetchData.tsx
+++ src/app/components/root/FetchData.tsx
@@ -11,12 +11,13 @@ type Data = {
 export default function FetchData() {
   useEffect(() => {
     async function fetchData() {
-      //   const data = await GraphQLV2(allItemsQuery);
-      const data = { items: [{ name: "Item 1" }, { name: "Item 2" }] } as Data;
+      const data = (await GraphQLV2(allItemsQuery)) as Data;
 
       if (data.errors) {
-        console.error(data.errors);
-        alert("Couldn't load items. Please refresh the page.");
+        data.errors.forEach((error) => {
+          console.error(error.message);
+        });
+        alert("Couldn't load items.");
         return;
       }
 
@@ -26,9 +27,6 @@ export default function FetchData() {
       );
 
       localStorage.setItem("items", JSON.stringify(allItems));
-      if (localStorage.getItem("items")) {
-        console.log(JSON.parse(localStorage.getItem("items") as string));
-      }
     }
 
     fetchData();
